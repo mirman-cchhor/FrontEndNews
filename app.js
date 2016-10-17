e2eea608abba875bb972fc138eaff090866dea44
Carolin <cchhor@mirman.org>
2016-10-17 04:58:32 -0700

    add modal overlay and styling transtions

--- app.js
+++ app.js
@@ -9,10 +9,24 @@ $(document).ready(function(){
     .addClass('is-liked');
   });
 
+
 $('.js-add-link').click(function(event){
     event.preventDefault();
 
     $('.js-form').toggleClass('is-visible');
   });
 
+
+// modal
+  $('.js-show-modal').click(function(event){
+    event.preventDefault();
+
+    $('.js-modal').addClass('is-visible');
+    $('.js-modal-overlay').addClass('is-visible');
+  });
+
+  $('.js-modal-overlay').click(function(event){
+    $('.js-modal').removeClass('is-visible');
+    $('.js-modal-overlay').removeClass('is-visible');
+  });
 });
